b9e4c9bfb88d0728729cf423e346db83a7f359a1
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-06-23 20:14:07 -0600

    Desafio 2

--- js/theJourneyBegins.js
+++ js/theJourneyBegins.js
@@ -5,3 +5,11 @@ function solution1( param1, param2) {
 
     return a + b;
 }
+
+/* 2) Given a year, return the century it is in. The first century spans from the year 1 up to and including
+ the year 100, the second from the year 101 up to and including the year 200, etc*/
+ function solution2(year) {
+    let century = Math.ceil(year / 100);
+
+    return century;
+ }
